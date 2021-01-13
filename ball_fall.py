b6354f074a1ec9abda558dfb9330b67e9eab6593
Sahil <sahilflash123@gmail.com>
2021-01-13 15:34:57 +0530

    Functional basic with graph

--- ball_fall.py
+++ ball_fall.py
@@ -1,12 +1,23 @@
+import matplotlib.pyplot as plt 
 import math
 
+#Standard Variables and inital values
 g = 9.8
 h = 4
+#t1 = time for the first hit to ground
 t1 = math.sqrt(2*h/g)
-print(t1)
-e = 0.9
+
+#coefficient of restitution
+e = 0.6
+
+# speed at the touchdown to ground for the first time
 v = math.sqrt(2*g*h)
 
+#X and Y coordinates list for the trajectory
+X=[0]
+Y=[4]
+
+#Static function to get the no. of times the ball has been hit before the given time
 def get_bounce_num(t):
     temp1 = 0
     temp = t1
@@ -17,22 +28,19 @@ def get_bounce_num(t):
         temp1 =temp
         temp =  temp + (pow(e , count+1))*t1*2
         count += 1
-    #print(count , temp1)
 
     return [count, temp1]
 
+#function to get x , y coordinates at a given instant of time
 def get_xy(t):
-    g = 9.8
-    h = 4
     x = 1*t
-    t1 = math.sqrt(2*h/g)
     y=0
     bounce = []
     time_interval = 0
     u = 0
 
     if(t<t1):
-        y = 1/2*g*t*t
+        y = 4 - 1/2*g*t*t
         return (x, y)
 
     else:
@@ -40,16 +48,18 @@ def get_xy(t):
         time_interval = t - bounce[1]
         u = pow(e , bounce[0])*v
         y = u*time_interval - (1/2)*g*time_interval*time_interval
-        #print("\n" , bounce)
         return (x, y)
 
+#time variable
+i=0
 
+#getting coordinates and adding it to LIST
+while(i<3.5):
+    coordinates = get_xy(i)
+    X.append(coordinates[0])
+    Y.append(coordinates[1])
+    i+=0.01
 
-print(get_xy(1))
-print(get_xy(2))
-print(get_xy(3))
-print(get_xy(4))
-print(get_xy(5))
-print(get_xy(6))
-print(get_xy(6.3))
-print(get_xy(6.4))
+#plotting the graph :)
+plt.plot(X, Y)
+plt.show()
